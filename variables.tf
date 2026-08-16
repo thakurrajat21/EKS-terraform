c66c72b429c223edb7fdc1a1e362a243a4a229bf
thakurrajat21 <122869012+thakurrajat21@users.noreply.github.com>
2026-08-17 00:42:03 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.28"
+  default     = "1.30"
 }
 
 variable "vpc_cidr" {
